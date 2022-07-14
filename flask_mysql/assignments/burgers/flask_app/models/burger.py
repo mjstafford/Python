ae9e888b3c531c0e497332c9f341750867d8a9f0
Michael <67665934+mjstafford@users.noreply.github.com>
2022-07-14 07:50:32 -0700

    updated burgers to connect to a db

--- flask_mysql/assignments/burgers/flask_app/models/burger.py
+++ flask_mysql/assignments/burgers/flask_app/models/burger.py
@@ -1,4 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+DATABASE = "login_reg"
 
 class Burger:
     def __init__(self,data):
@@ -9,16 +10,21 @@ class Burger:
         self.calories = data['calories']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.user_id = 1    # temporary!!!!!!!!!!!!!!! for testing otherwise fails since it has a foreign key in it
+                            # also had to create a first user in the users table in order to create burgers!
 
     @classmethod
     def save(cls,data):
-        query = "INSERT INTO burgers (name,bun,meat,calories,created_at,updated_at) VALUES (%(name)s,%(bun)s,%(meat)s,%(calories)s,NOW(),NOW())"
-        return connectToMySQL('burgers').query_db(query,data)
+        # query = "INSERT INTO burgers (name,bun,meat,calories,created_at,updated_at) VALUES (%(name)s,%(bun)s,%(meat)s,%(calories)s,NOW(),NOW())"
+        # ATTN!!!!!!!!! hardcoded in the user_id parameter and value. in order to allow creation of burgers during testing
+        query = "INSERT INTO burgers (name,bun,meat,calories,created_at,updated_at, user_id) VALUES (%(name)s,%(bun)s,%(meat)s,%(calories)s,NOW(),NOW(), 1)"
+        print("here")
+        return connectToMySQL(DATABASE).query_db(query,data)
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM burgers;"
-        burgers_from_db =  connectToMySQL('burgers').query_db(query)
+        burgers_from_db =  connectToMySQL(DATABASE).query_db(query)
         burgers =[]
         for b in burgers_from_db:
             burgers.append(cls(b))
@@ -27,16 +33,16 @@ class Burger:
     @classmethod
     def get_one(cls,data):
         query = "SELECT * FROM burgers WHERE burgers.id = %(id)s;"
-        burger_from_db = connectToMySQL('burgers').query_db(query,data)
+        burger_from_db = connectToMySQL(DATABASE).query_db(query,data)
 
         return cls(burger_from_db[0])
 
     @classmethod
     def update(cls,data):
         query = "UPDATE burgers SET name=%(name)s, bun=%(bun)s, meat=%(meat)s, calories=%(calories)s,updated_at = NOW() WHERE id = %(id)s;"
-        return connectToMySQL('burgers').query_db(query,data)
+        return connectToMySQL(DATABASE).query_db(query,data)
 
     @classmethod
     def destroy(cls,data):
         query = "DELETE FROM burgers WHERE id = %(id)s;"
-        return connectToMySQL('burgers').query_db(query,data)
+        return connectToMySQL(DATABASE).query_db(query,data)
