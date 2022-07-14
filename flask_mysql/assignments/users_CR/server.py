763b6aa7f7a32364789e602d135ef551412b3909
Michael <67665934+mjstafford@users.noreply.github.com>
2022-07-14 13:42:53 -0700

    udpated notes

--- flask_mysql/assignments/users_CR/server.py
+++ flask_mysql/assignments/users_CR/server.py
@@ -1,6 +1,6 @@
-#do we need this first import!?
+# we 100% need the first import because it basically injects all the routes back into this file
 from users_cr_app.controllers import users #this imports the users controller!
-from users_cr_app import app
+from users_cr_app import app    #this is because of the __init__ file under users_cr_app
 
 if __name__== "__main__":
     app.run(debug=True)
