f237148e6969df5c72e827613b2c897e3e26dbaa
Michael <67665934+mjstafford@users.noreply.github.com>
2022-07-16 20:37:29 -0700

    updated with flash/validation messages

--- flask/fundamentals/dojo_survey/server.py
+++ flask/fundamentals/dojo_survey/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request, redirect
+from flask import Flask, render_template, session, request, redirect, flash
 
 app = Flask(__name__)
 app.secret_key = "abc123"
@@ -9,6 +9,10 @@ def main_page():
 
 @app.route("/process", methods=["POST"])
 def post_page():
+    #first validate
+    if not validate_form(request.form):
+        return redirect("/")
+
     session["name"]= request.form["name"]
     session["dojo location"]= request.form["location"]
     session["favorite language"]= request.form["language"]
@@ -23,6 +27,22 @@ def result_page():
 def go_back():
     session.clear()
     return redirect("/")
+
+def validate_form(data):
+    is_valid = True
+    if len(data["name"]) <= 3:
+        flash("Name must be at least 3 chars long")
+        is_valid = False
+    if  0 < len(data["comment"]) < 3:
+        flash("comment must be at least 3 chars long")
+        is_valid = False
+    if "location" not in data:
+        flash("must select a location")
+        is_valid = False
+    if "language" not in data:
+        flash("must select a language")
+        is_valid = False
+    return is_valid
 
 if __name__ == "__main__":
     app.run(debug=True)
