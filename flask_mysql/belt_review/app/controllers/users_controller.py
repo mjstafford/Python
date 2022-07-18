beb0e67e07c4d26b0d072097c4c7019f822b606e
Michael <67665934+mjstafford@users.noreply.github.com>
2022-07-18 15:34:16 -0700

    recipe share page functionality done

--- flask_mysql/belt_review/app/controllers/users_controller.py
+++ flask_mysql/belt_review/app/controllers/users_controller.py
@@ -1,6 +1,28 @@
 from flask import Flask, redirect, render_template, session, flash, request
 from app import app
+from app.models import user
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+@app.route("/user/new", methods=["POST"])
+def create_user():
+    is_valid_user = user.User.validate_user(request.form)
+    print("is valid user",is_valid_user)
+    if not is_valid_user:
+        redirect("/")
+    
+    #if everything is good, save the user!
+    user.User.save(request.form)
+
+    return redirect("/")    #change to recipes page
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    is_valid_login = user.User.validate_login(request.form)
+    print("is valid login", is_valid_login)
+    if not is_valid_login:
+        redirect("/")
+    
+    return redirect("/")    #change to recipes page
---
+++ flask_mysql/belt_review/app/models/user.py
@@ -0,0 +1,85 @@
+from flask import flash
+from app.config.mysqlconnection import connectToMySQL
+from app import DATABASE,app
+import re
+
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+EMAIL_REGEX = re.compile(r'^[a-zA-z0-9.+_-]+@[a-zA-z0-9.+_-]+\.[a-zA-z]+$')
+
+
+class User:
+    def __init__(self, data):
+        self.id = data["id"],
+        self.first_name = data["first_name"],
+        self.last_name = data["last_name"],
+        self.email = data["email"],
+        self.password = data["password"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
+    @classmethod
+    def save(cls, data):
+        password_hashed = bcrypt.generate_password_hash(data["password"])
+        data = {
+            **data,
+            "password": password_hashed
+        }
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s);"
+        return connectToMySQL(DATABASE).query_db(query, data)
+
+
+
+    @classmethod
+    def find_user_by_email(cls, data):
+        query = "SELECT * FROM users WHERE users.email = %(email)s;"
+        result = connectToMySQL(DATABASE).query_db(query, data)
+        
+        if len(result) == 0:
+            return False
+
+        return User(result[0])
+
+    @staticmethod
+    def validate_user(data):
+        is_valid = True
+
+        if len(data["first_name"]) < 2:
+            flash("first name must be at least 2 characters", "register")
+            is_valid = False
+        if len(data["last_name"]) < 2:
+            flash("last name must be at least 2 characters", "register")
+            is_valid = False
+
+        if not EMAIL_REGEX.match(data["email"]):
+            flash("incorrect email format", "register")
+            is_valid = False
+
+        if data["password"] != data["password_confirm"]:
+            flash("passwords didn't match", "register")
+            is_valid = False
+
+        if User.find_user_by_email({"email": data["email"]}):  #must only send email
+            flash("user is already in system", "register")
+            is_valid = False
+
+        print("is valid:", is_valid)
+        return is_valid
+
+
+    @staticmethod
+    def validate_login(data):
+        is_valid = True
+
+        user = User.find_user_by_email({"email": data["email"]})
+
+        if not user:
+            flash("Invalid user/password", "login")
+            is_valid = False
+
+        elif not bcrypt.check_password_hash(user.password, data["password"]):
+            flash("Invalid user / password", "login")
+            is_valid = False
+
+        return is_valid
