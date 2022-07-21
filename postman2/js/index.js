973cfe9a0b673d7c5014db6213fb54597908bcbc
Michael <67665934+mjstafford@users.noreply.github.com>
2022-07-21 13:45:41 -0700

    postman example with api key

---
+++ postman2/js/index.js
@@ -0,0 +1,38 @@
+console.log("connection test")
+
+const API_KEY = "the_real_api_key_here"
+
+async function handleNewsRequest(event) {
+    event.preventDefault();
+
+    const searchTerm = document.querySelector("#searchTerm").value
+    const pageSize = document.querySelector("#pageSize").value
+    const URL = `https://newsapi.org/v2/everything?q=${searchTerm}&pagesize=${pageSize}`
+    console.log(URL)
+
+    //news api requires a header
+    const settings = {
+        method: "GET",
+        Headers: {
+            "x-api-key": API_KEY
+        }
+    }
+
+    const response = await fetch(URL, settings)
+    const data = await response.json()
+    console.log(data)
+
+    const results = document.querySelector(".results")
+    results.innerHTML = ""
+
+    for (const article of data.articles) {
+        results.innerHTML += `
+            <div class="article">
+                <h2>${article.title}</h2>
+                <img src=${article.urlToImage} />
+                <h5>${article.author}</h5>
+                <p>${article.description}</p>
+            </div>
+        `
+    }
+}
